1c595430ad6d8bbeac8e60274b6b1f9db9ce421d
Daniel Felipe Loaiza B <danielfloaiza@gmail.com>
2017-03-12 12:40:58 -0500

    increment reducer working

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,26 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+//import css
+import css from './styles/style.styl';
+// import components
+import App from './components/App'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid';
+import {Provider} from 'react-redux';
+import store, {history} from './store';
+
+// import react-router deps
+import {Router, Route, IndexRoute} from 'react-router'
+
+const router = (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App}>
+                <IndexRoute component={PhotoGrid}/>
+                <Route path="view/:postId" component={Single}/>
+            </Route>
+        </Router>
+    </Provider>
+)
+
+render(router, document.getElementById('root'));
